572e2f5eacd9b97ca6f98fe934b217f4ad0a06e0
Khorva <102830271+Khorva@users.noreply.github.com>
2022-04-01 23:19:20 -0500

    JTable Viewing
    Implemented a JTable onto the JScrollPane within the MainWin. Will need to implement another JDialog(or similar) for displaying full CE information.

--- FGO_ServantWiki/src/fgo_servantwiki/MainWin.java
+++ FGO_ServantWiki/src/fgo_servantwiki/MainWin.java
@@ -11,9 +11,15 @@ import javax.swing.JMenuItem;
 import javax.swing.JDialog;
 import javax.swing.JToolBar;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.DefaultTableModel;
         
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Image;
 
 public class MainWin extends JFrame {
     private Database database;
@@ -60,11 +66,23 @@ public class MainWin extends JFrame {
         this.getContentPane().add(toolbar, BorderLayout.NORTH);
        
         
-        //Testing Icon viewing functionality on JFrame  
-        //So far, only icons are show on the JFrame. the full portraits aren't 
-        JLabel full = new JLabel(this.database.getCE(1).getFullPicture());
+        //Testing Icon viewing functionality on JFrame
+        JLabel full = new JLabel(this.database.getCE(0).getFullPicture());
         JLabel icon = new JLabel(this.database.getCE(0).getIcon());
-        JScrollPane scrollpane = new JScrollPane(full);
+          
+        JTable ceTable = new JTable(database.getCEs().size(),3);
+        ceTable.getColumnModel().getColumn(1).setCellRenderer(new imageTableCellRenderer());
+        ceTable.setRowHeight(108);
+        
+        for(int i = 0; i < database.getCEs().size(); i++){
+            for(int j = 0; j < 3; j++){
+                if(j==0){ceTable.setValueAt(database.getCE(i).getID(),i,j);}
+                if(j==1){ceTable.setValueAt(database.getCE(i).getIcon(),i,j);}
+                if(j==2){ceTable.setValueAt(database.getCE(i).getName(),i,j);}
+            }
+        }
+        
+        JScrollPane scrollpane = new JScrollPane(ceTable);
         scrollpane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
         this.add(scrollpane);
         
@@ -79,5 +97,13 @@ public class MainWin extends JFrame {
         newCE.setTitle("Adding New CE");
         
         newCE.setVisible(true);
+    }
+    private class imageTableCellRenderer extends DefaultTableCellRenderer{
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int colum){
+            String imagePath = value.toString();
+            ImageIcon icon = new ImageIcon(new ImageIcon("src/" + imagePath).getImage().getScaledInstance(99,108,Image.SCALE_DEFAULT));
+            return new JLabel(icon);
+        }
     }
 }
